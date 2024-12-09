8f581cea4af289bb49c601b0d3d5af1128018448
전민오 <jbj338033@gmail.com>
2024-12-09 23:23:00 +0900

    feat: optimize archiving

--- src/components/home/Archiving/index.tsx
+++ src/components/home/Archiving/index.tsx
@@ -1,39 +1,64 @@
+import { memo } from "react";
 import * as S from "./style";
 import { BsGithub } from "react-icons/bs";
+import { IconType } from "react-icons";
 
-const ARCHIVE_DATA = [
+interface ArchiveItem {
+  title: string;
+  url: string;
+  description: string;
+  icon: IconType;
+}
+
+const ARCHIVE_DATA: readonly ArchiveItem[] = [
   {
     title: "GitHub",
     url: "github.com/jbj338033",
     description: "소스 코드 저장소",
-    icon: <BsGithub />,
+    icon: BsGithub,
   },
-];
+] as const;
+
+const Header = memo(() => (
+  <S.Header>
+    <S.Title>Archiving</S.Title>
+    <S.Description>저를 표현하는 다양한 공간입니다</S.Description>
+  </S.Header>
+));
+
+Header.displayName = "Header";
+
+const ArchiveCard = memo(({ item }: { item: ArchiveItem }) => {
+  const Icon = item.icon;
+
+  return (
+    <S.ArchiveCard
+      href={`https://${item.url}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <S.IconWrapper>
+        <Icon aria-hidden="true" />
+      </S.IconWrapper>
+      <S.TextContent>
+        <S.CardTitle>{item.title}</S.CardTitle>
+        <S.CardUrl>{item.url}</S.CardUrl>
+        <S.CardDescription>{item.description}</S.CardDescription>
+      </S.TextContent>
+    </S.ArchiveCard>
+  );
+});
+
+ArchiveCard.displayName = "ArchiveCard";
 
 const Archiving = () => {
   return (
     <S.Container id="archiving">
       <S.Inner>
-        <S.Header>
-          <S.Title>Archiving</S.Title>
-          <S.Description>저를 표현하는 다양한 공간입니다</S.Description>
-        </S.Header>
-
+        <Header />
         <S.Content>
           {ARCHIVE_DATA.map((item) => (
-            <S.ArchiveCard
-              key={item.title}
-              href={`https://${item.url}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <S.IconWrapper>{item.icon}</S.IconWrapper>
-              <S.TextContent>
-                <S.CardTitle>{item.title}</S.CardTitle>
-                <S.CardUrl>{item.url}</S.CardUrl>
-                <S.CardDescription>{item.description}</S.CardDescription>
-              </S.TextContent>
-            </S.ArchiveCard>
+            <ArchiveCard key={item.title} item={item} />
           ))}
         </S.Content>
       </S.Inner>
@@ -41,4 +66,4 @@ const Archiving = () => {
   );
 };
 
-export default Archiving;
+export default memo(Archiving);
