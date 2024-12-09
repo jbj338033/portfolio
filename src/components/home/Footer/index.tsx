b77b41e5af14d3dbedb892e9d97b92cab854184b
전민오 <jbj338033@gmail.com>
2024-12-09 23:25:49 +0900

    feat: optimize footer

--- src/components/home/Footer/index.tsx
+++ src/components/home/Footer/index.tsx
@@ -1,47 +1,76 @@
+import { memo } from "react";
 import * as S from "./style";
 import { BsGithub, BsInstagram, BsEnvelope } from "react-icons/bs";
+import { IconType } from "react-icons";
 
-const CONTACT_LINKS = [
+interface ContactLink {
+  icon: IconType;
+  href: string;
+  label: string;
+}
+
+const CONTACT_LINKS: readonly ContactLink[] = [
   {
-    icon: <BsGithub />,
+    icon: BsGithub,
     href: "https://github.com/jbj338033",
     label: "GitHub",
   },
   {
-    icon: <BsInstagram />,
+    icon: BsInstagram,
     href: "https://instagram.com/jbj338033",
     label: "Instagram",
   },
   {
-    icon: <BsEnvelope />,
+    icon: BsEnvelope,
     href: "mailto:jbj338033@gmail.com",
     label: "Email",
   },
-];
+] as const;
 
-const Footer = () => {
+const LinkItem = memo(({ link }: { link: ContactLink }) => {
+  const Icon = link.icon;
+
+  return (
+    <S.LinkItem
+      href={link.href}
+      target={link.href.startsWith("mailto") ? undefined : "_blank"}
+      rel={link.href.startsWith("mailto") ? undefined : "noopener noreferrer"}
+      aria-label={link.label}
+    >
+      <Icon aria-hidden="true" />
+    </S.LinkItem>
+  );
+});
+
+LinkItem.displayName = "LinkItem";
+
+const ContactLinks = memo(() => (
+  <S.ContactLinks>
+    {CONTACT_LINKS.map((link) => (
+      <LinkItem key={link.label} link={link} />
+    ))}
+  </S.ContactLinks>
+));
+
+ContactLinks.displayName = "ContactLinks";
+
+const Copyright = memo(() => {
   const currentYear = new Date().getFullYear();
 
+  return <S.Copyright>© {currentYear} JMO. All rights reserved.</S.Copyright>;
+});
+
+Copyright.displayName = "Copyright";
+
+const Footer = () => {
   return (
     <S.Container>
       <S.Inner>
-        <S.ContactLinks>
-          {CONTACT_LINKS.map((link) => (
-            <S.LinkItem
-              key={link.label}
-              href={link.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={link.label}
-            >
-              {link.icon}
-            </S.LinkItem>
-          ))}
-        </S.ContactLinks>
-        <S.Copyright>© {currentYear} JMO. All rights reserved.</S.Copyright>
+        <ContactLinks />
+        <Copyright />
       </S.Inner>
     </S.Container>
   );
 };
 
-export default Footer;
+export default memo(Footer);
